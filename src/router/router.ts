2d496e5ac72a0ec4fcfad2af335b8534458991be
tomychi <tomychi352@gmail.com>
2023-01-24 00:46:20 -0300

    config typeORM and docker-compose

---
+++ src/router/router.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+/**
+ * T controller
+ * U middleware
+ */
+export class BaseRouter<T> {
+  public router: Router;
+  public controller: T;
+  // public middleware: U
+
+  constructor(TController: { new (): T }) {
+    this.router = Router();
+    this.controller = new TController();
+    this.routes();
+  }
+
+  routes() {}
+}
