5799490e34ff15990f58a2bf6135bec28bd8f900
tomychi <tomychi352@gmail.com>
2023-01-25 00:38:06 -0300

    hashing password

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,3 @@
-import { Exclude } from 'class-transformer';
 import { Column, Entity, OneToOne } from 'typeorm';
 import { BaseEntity } from '../../config/base.entity';
 import { CustomerEntity } from '../../customer/entities/customer.entity';
@@ -18,8 +17,7 @@ export class UserEntity extends BaseEntity {
   @Column()
   email!: string;
 
-  @Exclude()
-  @Column()
+  @Column({ select: false })
   password!: string;
 
   @Column()
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,5 @@
 import { DeleteResult, UpdateResult } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { BaseService } from '../../config/base.service';
 import { UserDTO } from '../dto/user.dto';
 import { UserEntity } from '../entities/user.entity';
@@ -24,8 +25,33 @@ export class UserService extends BaseService<UserEntity> {
       .getOne();
   }
 
+  // obtener usuario por email
+  async findUserByEmail(email: string): Promise<UserEntity | null> {
+    return (await this.execRepository)
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where({ email })
+      .getOne();
+  }
+
+  // obtener usuario por username
+  async findUserByUsername(username: string): Promise<UserEntity | null> {
+    return (await this.execRepository)
+      .createQueryBuilder('user')
+      .addSelect('user.password')
+      .where({ username })
+      .getOne();
+  }
+
   async createUser(body: UserDTO): Promise<UserEntity> {
-    return (await this.execRepository).save(body);
+    // create se guarda en memoria
+    const newUser = (await this.execRepository).create(body);
+
+    const hash = await bcrypt.hash(newUser.password, 10);
+
+    newUser.password = hash;
+
+    return (await this.execRepository).save(newUser);
   }
   async deleteUser(id: string): Promise<DeleteResult> {
     return (await this.execRepository).delete({ id });
