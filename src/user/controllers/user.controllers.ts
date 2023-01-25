bfbd5f74371ebe65f9b480b8e7d09d9096cca8c6
tomychi <tomychi352@gmail.com>
2023-01-24 23:54:57 -0300

    relation user-customer

--- src/user/controllers/user.controllers.ts
+++ src/user/controllers/user.controllers.ts
@@ -38,6 +38,21 @@ export class UserController {
     }
   }
 
+  async getUserWithRelationById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const data = await this.userService.findUserWithRelation(id);
+
+      if (!data) {
+        return this.httpResponse.NotFound(res, 'No existen datos');
+      }
+
+      return this.httpResponse.Ok(res, data);
+    } catch (e) {
+      return this.httpResponse.Error(res, e);
+    }
+  }
+
   async createUser(req: Request, res: Response) {
     try {
       const data = await this.userService.createUser(req.body);
